d5b1428c76077e788047b41bd6513c6dfc710cfe
DESKTOP-DKSIK09\Admin <brzden32@gmail.com>
2021-09-08 12:30:02 -0300

    enable https on backend

--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,10 @@ export const ALGORAND_LEDGER = 'TestNet';
 export const USDC_ID = 14001707;
 export const USDC_DECIMAL_POINTS = 2;
 // export const ASSET_URL = 'nft-market.ulam.io';
-export const ASSET_URL = '34.134.119.115';
+export const ASSET_URL = 'https://34.134.119.115';
 export var BACKEND_URL;
 if (process.env.NODE_ENV === 'development') {
   BACKEND_URL = 'http://localhost:8000';
 } else {
-  BACKEND_URL = 'http://34.134.119.115';
+  BACKEND_URL = 'https://34.134.119.115';
 }
